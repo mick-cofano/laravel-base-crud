23550a9fa0e5b644b2a2334cdf7c85a37cf26e1f
Michele Cofano <brutalmachinelol@gmail.com>
2021-03-18 20:35:10 +0100

    small update, created CRUD

--- laravel-base-crud/routes/web.php
+++ laravel-base-crud/routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', 'BaseController@index');
+
+
+Route::resource('/beers', 'BeerController');
